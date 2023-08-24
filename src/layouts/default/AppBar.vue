132c01c8a9e4c6bd4a943355e9795d6b3207d9e1
Marius Minde <marius.mind@gmail.com>
2023-08-24 15:52:50 +0200

    Started frontpage

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,53 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
-    </v-app-bar-title>
+  <v-app-bar flat height="150">
+    <v-app-bar-title>Stine og Marius</v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+//
 </script>
+
+<style scoped>
+header.v-app-bar {
+  background-color: #b2ac88;
+  color: #000000;
+}
+</style>
+
+<style>
+@font-face {
+  font-family: "I Love Glitter";
+  src: url("/public/fonts/I Love Glitter.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Monsieur-Regular";
+  src: url("/public/fonts/MonsieurLaDoulaise-Regular.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "The heart of everything";
+  src: url("/public/fonts/The Heart of Everything - TTF.ttf") format("truetype"),
+    url("/public/fonts/The Heart of Everything - OTF.otf") format("otf");
+}
+
+@font-face {
+  font-family: "Brotherhood";
+  src: url("/public/fonts/Brotherhood_Script.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Andasia";
+  src: url("/public/fonts/Andasia.ttf") format("truetype");
+}
+
+.v-toolbar__content .v-toolbar-title__placeholder {
+  font-family: "Andasia", sans-serif;
+  font-size: 5rem;
+  font-weight: 100;
+  text-align: center;
+  overflow: visible;
+  height: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <HelloWorld />
+  <div class="full-width background-image">
+    <v-container>
+      <v-row>
+        <v-col cols="12"> </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+<script setup></script>
+
+<style lang="less" scoped>
+.background-image {
+  background-image: url("https://s3.eu-west-1.amazonaws.com/mariusminde.no/stineogmarius/Tyrkia2013-006.jpg");
+  background-color: #b2ac88;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: calc(~"100vh - 150px");
+  width: 100vw;
+  position: relative;
+}
+</style>
