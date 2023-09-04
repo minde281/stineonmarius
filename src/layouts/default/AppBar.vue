8dbbad902c809c7f701439fae71d7a65bd2e0718
Marius Minde <marius.mind@gmail.com>
2023-09-04 23:59:45 +0200

    Added music

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,15 +1,59 @@
 <template>
   <v-app-bar flat height="150">
     <v-app-bar-title>Stine og Marius</v-app-bar-title>
+    <div class="music">
+      <div v-if="musicPlaying" class="v-btn toggle-music" @click="stopMusic">
+        <v-icon icon="mdi-music-note"></v-icon>
+      </div>
+      <div v-else class="v-btn toggle-music" @click="playMusic">
+        <v-icon icon="mdi-music-note-off"></v-icon>
+      </div>
+    </div>
   </v-app-bar>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "AppBar",
+  data() {
+    return {
+      musicPlaying: false,
+      music: new Audio(
+        "https://s3.eu-west-1.amazonaws.com/mariusminde.no/stineogmarius/Snow+Patrol+-+Chasing+Cars+-+Nick+Pitera+(cover).mp3"
+      ),
+    };
+  },
+  created() {
+    this.music.addEventListener("ended", () => {
+      this.musicPlaying = false;
+      this.music.currentTime = 0;
+    });
+  },
+  methods: {
+    playMusic() {
+      this.musicPlaying = true;
+      this.music.play();
+    },
+    stopMusic() {
+      this.musicPlaying = false;
+      this.music.pause();
+    },
+  },
+};
+</script>
 
 <style scoped>
 header.v-app-bar {
   background-color: #b2ac88;
   color: #000000;
+}
+
+.music {
+  position: fixed;
+  right: 30px;
+  top: 40px;
+  margin: 1rem;
+  font-size: 20px;
 }
 </style>
 
