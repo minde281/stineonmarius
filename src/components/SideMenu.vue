ba385110df4208d7dd39855faee7b6995059b767
Marius Minde <marius.mind@gmail.com>
2024-01-02 10:42:45 +0100

    SideMenu not in use?

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div name="side-menu" class="side-menu">
+    <v-list>
+      <router-link to="/"><v-list-item link>Home</v-list-item></router-link>
+      <v-divider></v-divider>
+      <router-link to="/new-home">
+        <v-list-item link>New home</v-list-item>
+      </router-link>
+      <router-link to="/program">
+        <v-list-item link>Program</v-list-item>
+      </router-link>
+      <router-link to="/new-home">
+        <v-list-item link>New home</v-list-item>
+      </router-link>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "side-menu",
+  components: {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.side-menu {
+  a {
+    text-decoration: none;
+  }
+}
+</style>
