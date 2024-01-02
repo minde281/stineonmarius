72d752eb52214e9d58987cb0c1c3f1bd62f63252
Marius Minde <marius.mind@gmail.com>
2024-01-02 10:39:37 +0100

    Added more pages

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,30 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/new-home',
+        name: 'New Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/NewHome.vue'),
+      },
+      {
+        path: '/program',
+        name: 'Program',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Program.vue'),
+      },
+      {
+        path: '/faq',
+        name: 'FAQ',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/FAQ.vue'),
+      },
     ],
   },
 ]
