0471f7d07f4e7f9805c8dbf4e13924739ca9b8aa
Marius Minde <marius.mind@gmail.com>
2024-01-03 18:46:16 +0100

    Added new View: Forlovere

---
+++ src/views/Forlovere.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="full-width">
+    <div class="faq-holder">
+      <div class="center-text">
+        <div class="faq">
+          <h1 class="header">Forlovere</h1>
+          <div class="section forlovere">
+            <v-row>
+              <v-col class="forlover">
+                <p>Sandra Gundersen</p>
+                <p></p>
+              </v-col>
+              <v-col class="forlover">
+                <p>Tom Erik Berger</p>
+                <p></p>
+              </v-col>
+            </v-row>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+@background-color: #ffffff;
+@text-box-background-color: #b2ac88;
+@text-box-text-color: #000000;
+@text-box-text-alt-color: #c9b277;
+@default-font: "Alexis Marie", sans-serif;
+@header-font: "Forum", sans-serif;
+</style>
