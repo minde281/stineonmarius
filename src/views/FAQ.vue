0748d0a80aeb6b4a3fe163012c8ac941a838b492
Marius Minde <marius.mind@gmail.com>
2024-01-03 19:59:53 +0100

    FAQ

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,21 +1,185 @@
 <template>
   <div class="full-width">
-    <div class="faq-holder">
+    <div class="container faq-holder">
       <div class="center-text">
         <div class="faq">
-          <h1 class="header">FAQ</h1>
-          <div class="section forlovere">
-            <h2>Forlovere</h2>
-            <v-row>
-              <v-col class="forlover">
-                <p>Sandra Gundersen</p>
-                <p></p>
-              </v-col>
-              <v-col class="forlover">
-                <p>Tom Erik Berger</p>
-                <p></p>
-              </v-col>
-            </v-row>
+          <h1 class="header">Spørsmål og svar</h1>
+          <div class="section dress-code">
+            <h2>Hva er kleskoden?</h2>
+            <p>Finn frem finstasen; Kleskoden er Mørk Dress</p>
+            <p>
+              Denne kleskoden innebærer dress i mørk blå, grå eller sort, enten
+              ensfarget eller med diskré striper eller lignende. Kvinner kan
+              bruke kort eller lang kjole eller skjørt, eller en pen langbukse
+              med en pyntet overdel til. Det er helt i orden å komme i svart.
+            </p>
+          </div>
+          <div class="section vielse">
+            <h2>Når og hvor er vielsen?</h2>
+            <p>
+              Vielsen starter kl. 13:00 og finner sted på Rotvoll Park (Bygg D,
+              som ligger på baksiden av området). Vi anbefaler derfor at folk
+              kommer rundt 12:30.
+              <br /><a
+                target="_blank"
+                href="https://www.google.com/maps/place/Arkitekt+Ebbells+veg+22,+7053+Ranheim/@63.4390139,10.4835371,17z/data=!4m6!3m5!1s0x466d30f0e9d0e49b:0xbd66a809bba86067!8m2!3d63.4404874!4d10.4836778!16s%2Fg%2F11c1dtj17_?entry=ttu"
+                ><img
+                  class="map-image"
+                  src="https://s3.eu-west-1.amazonaws.com/mariusminde.no/stineogmarius/Screenshot+2024-01-03+185651.png"
+                  alt="Kart - Rotvoll Park"
+              /></a>
+            </p>
+          </div>
+          <div class="section outdoor">
+            <h2>Er vielsen utendørs?</h2>
+            <p>
+              Ettersom vielsen holdes på Rotvoll Park tror flere at vi skal være
+              utendørs, men Rotvoll Park er navnet på området. Vi skal være inne
+              i Bygg D
+            </p>
+          </div>
+          <div class="section parking-rotvoll">
+            <h2>Er det mulig å parkere på Rotvoll Park?</h2>
+            <p>
+              På Rotvoll er det stor parkeringsplass som drives av Apcoa. For å
+              komme hit holder man til høyre forbi hovedhuset, så er den til
+              venstre. Det blir servert bobler og kake etter vielsen
+            </p>
+          </div>
+          <div class="section pictures">
+            <h2>Kan jeg ta bilder under vielsen?</h2>
+            <p>
+              Under vielsen ønsker vi ingen mobiltelefoner, som vil si ingen
+              bilder. Det skal være en egen fotograf som tar bilder av vielsen,
+              så da vil vi ikke ha bilder av at folk holder frem mobilen sin.
+              Det vil bli anledning til å ta bilder før vielsen starter. Vi
+              oppfordrer likevel alle til å ta masse bilder etter vielsen og på
+              selskapet
+            </p>
+          </div>
+          <div class="section rotvoll-til-scandic">
+            <h2>Hva gjør vi i tiden mellom sermoni og fest?</h2>
+            <p>
+              I tiden mellom vielsen og selskapet kan de som ønsker det være på
+              Rotvoll Park. Der holdes det åpent frem til kl 16.00
+            </p>
+          </div>
+          <div class="section selskap-oppmote">
+            <h2>Når og hvor er selskapet?</h2>
+            <p>
+              Middagen serveres ca. kl 17:00. Man bør derfor møte 16:30.
+              Selskapet finner sted på
+              <a
+                target="_blank"
+                href="https://www.google.com/maps/place/Scandic+Nidelven/@63.4355593,10.4040406,17z/data=!3m1!4b1!4m9!3m8!1s0x466d319ef6092a6d:0x9e0dc238b05be471!5m2!4m1!1i2!8m2!3d63.4355569!4d10.4066155!16s%2Fg%2F11b6nncn1b?entry=ttu"
+              >
+                Scandic Nidelven
+                <img
+                  class="map-image"
+                  src="https://s3.eu-west-1.amazonaws.com/mariusminde.no/stineogmarius/Screenshot+2024-01-03+191621.png"
+                  alt="Kart - Scandic Nidelven"
+                />
+              </a>
+              <br />Selskapet holdes i innerste del av restauranten.
+            </p>
+          </div>
+          <div class="section overnatting">
+            <h2>Kan vi overnatte på Scandic Nidelven?</h2>
+            <p>
+              De som ønsker å overnatte på Scandic Nidelven kan få rabattert rom
+              ved å bruke bookingkode <strong>BSTI220624</strong> innen 07. juni
+              2024. Man kan bestille rom på følgende måter:
+            </p>
+            <ul>
+              <li>
+                Direkte til hotellet via telefon
+                <a href="tel:73568002">73 56 80 02</a>
+              </li>
+              <li>
+                Direkte til hotellet via mail
+                <a href="mailto:nidelven@scandichotels.com"
+                  >nidelven@scandichotels.com</a
+                >
+              </li>
+              <li>
+                Via hotellets hjemmeside,
+                <a href="https://www.scandichotels.com/nidelven"
+                  >www.scandichotels.com/nidelven</a
+                >
+                <br />Husk å legge bookingkoden i riktig felt
+              </li>
+            </ul>
+            <br />
+            <p>
+              Det er et begrenset antall rom, så fint om koden ikke deles videre
+            </p>
+          </div>
+          <div class="section parking-hotel">
+            <h2>Hvor kan jeg parkere for selskapet?</h2>
+            <p>
+              Nærmeste parkering til Scandic Nidelven er parkeringshuset ved
+              Trondheim Sentralstasjon eller parkeringshuset ved Olavskvartalet
+            </p>
+          </div>
+          <div class="section speeches">
+            <h2>Kan jeg holde tale eller sjonglere?</h2>
+            <p>
+              Vi ønsker at bryllupet skal være underholdene for alle og vil
+              derfor at personlige taler skal begrenses. Dersom noen likevel
+              ønsker å holde en tale eller arrangere annen aktivitet er det fint
+              om det er underholdene for alle.
+            </p>
+            <p>
+              Ønsker du holde tale kan du kontakte toastmaster
+              Thomas&nbsp;Minde&nbsp;Isaksen på telefon
+              <a href="tel:94059295">94059295</a>
+            </p>
+          </div>
+          <div class="section wishlist">
+            <h2>Har dere ønskeliste?</h2>
+            <p>
+              Vi er heldige og har alt vi trenger av materielle ting. Det vi
+              skulle hatt mer av er tid sammen. Vi ønsker oss derfor et bidrag
+              til bryllupsreise
+            </p>
+          </div>
+          <div class="section music">
+            <h2>Kan vi komme med musikkønsker?</h2>
+            <p>
+              Hjelp oss gjerne med å lage spilleliste for kvelden. Har du en
+              sang du vil høre kan du legge den til på
+              <a
+                target="_blank"
+                href="https://open.spotify.com/playlist/2gmiGRcHdP1dnw4x6q5pvE?si=9a1503f03c6c4f96"
+                >spotifylista vår</a
+              >
+            </p>
+          </div>
+          <div class="section photos">
+            <h2>
+              Jeg tok et sinnsykt morsomt bilde under selskapet. Hvordan deler
+              jeg dette?
+            </h2>
+            <p>
+              Vi ønsker å samle alle bilder fra dagen. Har du tatt noen bilder
+              kan du laste de opp
+              <a
+                target="_blank"
+                href="https://photos.app.goo.gl/kpox96iZmP6dMSwB7"
+                >her</a
+              >
+            </p>
+          </div>
+          <div class="section rsvp">
+            <h2>Når må jeg svare innen?</h2>
+            <p>
+              Gi gjerne beskjed om dere kommer eller ikke så snart som mulig,
+              men senest innen 15. april
+            </p>
+          </div>
+          <div class="section more-questions">
+            <h2>Flere spørsmål?</h2>
+            <p>Hvis dere har flere spørsmål er det bare å kontakte oss</p>
           </div>
         </div>
       </div>
@@ -38,4 +202,59 @@ export default {
 @text-box-text-alt-color: #c9b277;
 @default-font: "Alexis Marie", sans-serif;
 @header-font: "Forum", sans-serif;
+
+.faq-holder {
+  max-width: 80vw;
+  .center-text {
+    font-family: @default-font;
+    text-align: center;
+    a {
+      color: @text-box-text-alt-color;
+    }
+    ul {
+      max-width: 600px;
+      margin: 0 auto;
+      margin-top: -2rem;
+      li {
+        text-align: left;
+      }
+    }
+    .section {
+      margin-bottom: 2rem;
+      h2 {
+        font-family: @header-font;
+        color: @text-box-text-alt-color;
+        font-size: 36px;
+        font-weight: 200;
+      }
+      p {
+        font-size: 20px;
+        font-weight: 200;
+        margin-bottom: 2rem;
+        strong {
+          font-weight: 600;
+        }
+      }
+    }
+  }
+}
+.map-image {
+  max-width: 100%;
+  max-height: 80vh;
+}
+@media screen and (max-width: 480px) {
+  .faq-holder {
+    max-width: 90vw;
+    .center-text {
+      .section {
+        h2 {
+          font-size: 24px;
+        }
+        p {
+          font-size: 16px;
+        }
+      }
+    }
+  }
+}
 </style>
